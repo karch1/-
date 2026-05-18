4077565ede531041d6e4ab235536f3d461fe096a
karch1 <rkdeotjdak@naver.com>
2026-05-18 13:23:22 +0900

    11123123
    1111

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,49 @@
 self.addEventListener("install",(event)=>{
-
     console.log("Service Worker Installed");
 });
 
 self.addEventListener("activate",(event)=>{
-
     console.log("Service Worker Activated");
 });
 
 self.addEventListener("fetch",(event)=>{
+});
 
+/* =========================
+   FCM BACKGROUND PUSH
+========================= */
+
+importScripts(
+  'https://www.gstatic.com/firebasejs/12.13.0/firebase-app-compat.js'
+);
+
+importScripts(
+  'https://www.gstatic.com/firebasejs/12.13.0/firebase-messaging-compat.js'
+);
+
+firebase.initializeApp({
+
+    apiKey: "AIzaSyBU3jeOqj_FCBXeO2ghh5XlHBrQbUjNaiA",
+    authDomain: "ahn-jin-zero.firebaseapp.com",
+    projectId: "ahn-jin-zero",
+    storageBucket: "ahn-jin-zero.firebasestorage.app",
+    messagingSenderId: "55250475892",
+    appId: "1:55250475892:web:c87555da879be518faaa21",
+    measurementId: "G-V2TEL4TNPJ"
+});
+
+const messaging = firebase.messaging();
+
+/* 백그라운드 푸시 */
+messaging.onBackgroundMessage((payload) => {
+
+    console.log("백그라운드 알림", payload);
+
+    self.registration.showNotification(
+        payload.notification.title,
+        {
+            body: payload.notification.body,
+            icon: "./icon-192.png"
+        }
+    );
 });
